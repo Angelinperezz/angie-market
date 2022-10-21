dd77756f759db0db5d5af2bfa132cdbef19d0846
angelinperezz <109384672+Angelinperezz@users.noreply.github.com>
2022-10-21 18:57:35 -0400

    Cambios para el despliegue de heroku

---
+++ src/main/java/com/angiemarket/AngieMarketApplication.java
@@ -0,0 +1,13 @@
+package com.angiemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngieMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngieMarketApplication.class, args);
+	}
+
+}
